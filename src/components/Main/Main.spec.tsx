a75bf0b77ce969c342540271f0438214fc89e356
Weslley Silva <weslleysilva@DESKTOP-J958K0H.localdomain>
2021-04-04 20:20:50 -0300

    First test

--- src/components/Main/Main.spec.tsx
+++ src/components/Main/Main.spec.tsx
@@ -3,10 +3,12 @@ import Main from './Main'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    render(<Main />)
+    const { container } = render(<Main />)
 
     expect(
       screen.getByRole('heading', { name: /weslley silva/i })
     ).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 })
